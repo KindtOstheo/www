60d657fc6a3709e78796c72a81a97844ae287544
BlackWolfie <lucvdv83@hotmail.fr>
2023-06-29 16:00:35 +0200

    correct blockquote

--- layouts/shortcodes/Blockquote.jsx
+++ layouts/shortcodes/Blockquote.jsx
@@ -3,9 +3,9 @@ import { TfiQuoteLeft } from "react-icons/tfi";
 const Blockquote = ({ name, children }) => {
   return (
     <blockquote className="rounded-xl border border-border-secondary bg-body px-8 py-3  not-italic">
-      <span className="text-5xl text-gray-400">
+      {/* <span className="text-5xl text-gray-400">
         <TfiQuoteLeft />
-      </span>
+      </span> */}
       {children}
       <span className="m-0 block border-t border-border-secondary pt-3 text-base font-normal text-text after:hidden">
         {name}
